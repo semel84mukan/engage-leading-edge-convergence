8105808b2b39ddea44d4c0d4991139dab847b843
semel84mukan <152295085+semel84mukan@users.noreply.github.com>
2023-12-02 01:20:53 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,40 @@
+import random
+def main():
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+def generate_random_data():
+
+    data = generate_random_data()
+
+    main()
+    return data    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+    return data
+import random
+if __name__ == "__main__":
+    main()
+
+
+def main():
+def generate_random_data():    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+import random
+
+if __name__ == "__main__":
+    for item in data:
+def main():
+    return data
+
+    main()
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
